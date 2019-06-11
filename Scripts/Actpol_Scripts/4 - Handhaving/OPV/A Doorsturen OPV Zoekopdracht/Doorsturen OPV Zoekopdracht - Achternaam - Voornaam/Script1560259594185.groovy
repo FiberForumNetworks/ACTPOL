bca64d3068a3c91cd8891d54f67a8e58767d08b8
Nivesol <nagulmeeravali@nivesol.com>
2019-06-11 21:44:52 +0530

    i made few changes in Doorsturen OPV Zoekopdracht

---
+++ Scripts/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Achternaam - Voornaam/Script1560259594185.groovy
@@ -0,0 +1,69 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('main/Actpol_Login_testenv'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.mouseOver(findTestObject('Actpol_Scripts/Menu navigation/Handhaving/Handhaving'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Actpol_Scripts/Menu navigation/Handhaving/Doorsturen OPV'))
+
+WebUI.delay(2)
+
+not_run: WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Start Datum'), '26/02/2012')
+
+not_run: WebUI.delay(1)
+
+not_run: WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Eind Datum'), '26/02/2014')
+
+not_run: WebUI.delay(1)
+
+not_run: WebUI.selectOptionByValue(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Wijkteam'), 
+    '1', false)
+
+not_run: WebUI.delay(1)
+
+not_run: WebUI.clearText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Teamleiding naam'), 
+    FailureHandling.STOP_ON_FAILURE)
+
+not_run: WebUI.delay(1)
+
+not_run: WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Teamleiding naam'), 'JANSEN')
+
+not_run: WebUI.delay(2)
+
+not_run: WebUI.click(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Teamleiding naam lb 2'))
+
+WebUI.delay(1)
+
+WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Achternaam'), 'O\'stiana')
+
+WebUI.delay(1)
+
+WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Voornaam'), 'CLEOTIS')
+
+not_run: WebUI.delay(1)
+
+not_run: WebUI.setText(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/KC nummer'), '10548')
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Actpol_Scripts/Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Filter'))
+
+WebUI.delay(2)
+
+WebUI.takeScreenshot()
+
---
+++ Test Suites/Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Doorsturen OPV Zoekopdracht</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>80e72db1-2031-4787-88fd-3ee62ea44650</testSuiteGuid>
+   <testCaseLink>
+      <guid>8248eea4-fea0-4043-9000-69a5fa3ea20d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d739daf4-3680-4628-867f-30b42b48994b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Eind Datum</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a2408a86-f70b-45fd-ac33-8fcde6942fc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Wijkteam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4cee14a-31fa-427a-bc58-c1cfb873babe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Teamleiding naam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71a77be9-1307-4e3e-bc7c-9f5a5b57f4ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Achternaam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2de5d29-9f7f-4d61-a3e2-ca5f7720b298</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Voornaam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7593b10-5764-4592-89d2-e9952bc3104a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - KC nummer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c40c525-3040-420b-a675-843b5b2e2384</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum and Eind Datum</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1223217-61c3-46eb-9120-c6a735edb837</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum,Eind Datum and Wijkteam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>39e5e6a4-6ba4-4696-a775-b8c443798c2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum,Eind Datum,Wijkteam and Teamleiding naam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36a3736f-8811-492a-ad97-9b7190d9b3b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum,Eind Datum,Wijkteam,Teamleiding naam and Achternaam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>725bcbe3-b463-426d-94b4-e3c73e46f3ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum,Eind Datum,Wijkteam,Teamleiding naam,Achternaam and Voornaam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f188e7f-b920-4339-9b6a-aa74656f9271</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Actpol_Scripts/4 - Handhaving/OPV/A Doorsturen OPV Zoekopdracht/Doorsturen OPV Zoekopdracht - Start Datum,Eind Datum,Wijkteam,Teamleiding naam,Achternaam,Voornaam and KC nummer</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
